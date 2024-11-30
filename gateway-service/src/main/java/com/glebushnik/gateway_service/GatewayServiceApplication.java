450a919b5dce12426fa471fbba3440c78f48f6b1
glebushnik <glebushnik@MacBook-Pro-glebushnik.local>
2024-11-30 16:00:40 +0300

    babushka

--- gateway-service/src/main/java/com/glebushnik/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/glebushnik/gateway_service/GatewayServiceApplication.java
@@ -3,10 +3,12 @@ package com.glebushnik.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
--- service-register/src/main/java/com/example/service_register/ServiceRegisterApplication.java
+++ service-register/src/main/java/com/example/service_register/ServiceRegisterApplication.java
@@ -3,9 +3,11 @@ package com.example.service_register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableEurekaServer
 @SpringBootApplication
+@CrossOrigin
 public class ServiceRegisterApplication {
 
 	public static void main(String[] args) {
