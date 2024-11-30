f0f78ba1c11077bde1b66290551f64aacd6da0c2
glebushnik <glebushnik@MacBook-Pro-glebushnik.local>
2024-11-30 15:18:44 +0300

    gateway config

--- gateway-service/src/main/java/com/glebushnik/gateway_service/OpenAPIConfig.java
+++ gateway-service/src/main/java/com/glebushnik/gateway_service/OpenAPIConfig.java
@@ -1,13 +1,18 @@
 package com.glebushnik.gateway_service;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/myapp/", description = "Default Server URL")
+        }
+)
 public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
