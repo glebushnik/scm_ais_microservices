51730a9b1db8a4c2022add9ed53976b7be39719f
glebushnik <glebushnik@MacBook-Pro-glebushnik.local>
2025-01-07 02:19:07 +0300

    fix: dockerfile build profile

--- warehouse-service/src/main/java/com/glebushnik/warehouse_service/config/OpenApiConfig.java
+++ warehouse-service/src/main/java/com/glebushnik/warehouse_service/config/OpenApiConfig.java
@@ -23,7 +23,7 @@ import org.springframework.context.annotation.Configuration;
                 description = "Warehouse service"
         ),
         servers = {
-                @Server(url = "http://localhost:8080", description = "Gateway Server"),
+                @Server(url = "http://gateway-service:8080", description = "Gateway Server"),
         }
 )
 @SecurityScheme(
